12e5a79d06742751af01f70f01d032aaeaf77c05
james <james.maxwell.hu@gmail.com>
2015-03-04 12:39:03 +0800

    add ex1~4

---
+++ ex1.rb
@@ -0,0 +1,8 @@
+#this is comment
+puts "Hello Ruby!"
+puts "Hello World!"
+puts "Hello Again"
+puts "I like typing this."
+puts 'Yay! Printing.'
+puts "I's much rather you 'not'."
+puts 'I " said" to not touch this.'
